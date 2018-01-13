867d35b9a606c1731bdfc06f0e0c81c477856e89
Weeraphat <Donutza14@hotmail.com>
2018-01-13 15:31:16 +0700

    Create Alert Dialog

--- app/src/main/java/weeraphat/sbc/ac/th/sbc_moneychange/fragment/CalculateFragment.java
+++ app/src/main/java/weeraphat/sbc/ac/th/sbc_moneychange/fragment/CalculateFragment.java
@@ -1,13 +1,17 @@
 package weeraphat.sbc.ac.th.sbc_moneychange.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import weeraphat.sbc.ac.th.sbc_moneychange.MainActivity;
 import weeraphat.sbc.ac.th.sbc_moneychange.R;
@@ -37,11 +41,60 @@ public class CalculateFragment extends Fragment {
         toolbarContronler();
 
         //Get Value From Argument
-        aDouble = getArguments().getDouble("Factor");
-        Log.d("13Jan", "Factor ==>" + aDouble);
+        getValueFromArgument();
+
+//        Calculate Controler
+        calculateControler();
 
 
     }//Main Metthod
+
+    private void calculateControler() {
+        Button button = getView().findViewById(R.id.btncalculat);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Get Value From EditText
+                EditText editText = getView().findViewById(R.id.edtMoney);
+                String moneyString = editText.getText().toString().trim();
+
+//                Check Space
+                if (moneyString.isEmpty()) {
+//                    Have Space
+                    myAletr("Have Space", "Please Fill Money in Blank");
+
+
+                } else {
+//                    No Space
+
+                }
+
+            }//onClick
+        });
+
+    }
+
+    private void myAletr(String titleString, String messageString) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("You OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }   //End MyAletr
+
+    private void getValueFromArgument() {
+        aDouble = getArguments().getDouble("Factor");
+        Log.d("13Jan", "Factor ==>" + aDouble);
+    }
 
     private void toolbarContronler() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarcalculate);
